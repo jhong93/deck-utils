af3ea35620f5a782a380c656b03ed19c120852d0
James Hong <james.hong@cs.stanford.edu>
2022-09-23 13:44:49 -0700

    Add glibc

--- pacman_setup.sh
+++ pacman_setup.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-# sudo pacman-key --populate archlinux
-# sudo pacman -Sy archlinux-keyring
+sudo steamos-readonly disable
 
-sudo pacman -S openconnect imagemagick evince opera opera-ffmpeg-codecs neofetch stress
+sudo pacman-key --populate archlinux
+sudo pacman -Sy archlinux-keyring
+
+sudo pacman -S gcc cmake glibc linux-api-headers
+
+sudo pacman -S openconnect imagemagick evince neofetch stress net-tools traceroute
+
+sudo pacman -S opera opera-ffmpeg-codecs
