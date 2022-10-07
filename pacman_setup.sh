6ecf023d38b31660a40952b2c85925f620f35932
James Hong <james.hong@cs.stanford.edu>
2022-10-06 19:14:59 -0700

    Fix pacman script

--- pacman_setup.sh
+++ pacman_setup.sh
@@ -2,11 +2,14 @@
 
 sudo steamos-readonly disable
 
+sudo pacman-key --init
 sudo pacman-key --populate archlinux
 sudo pacman -Sy archlinux-keyring
 
 sudo pacman -S gcc cmake glibc linux-api-headers
 
-sudo pacman -S openconnect imagemagick evince neofetch stress net-tools traceroute
+sudo pacman -S --overwrite "/etc/vpnc/vpnc-script" openconnect
+
+sudo pacman -S imagemagick evince neofetch stress net-tools traceroute
 
 sudo pacman -S opera opera-ffmpeg-codecs
