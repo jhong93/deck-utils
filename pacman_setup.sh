ebc600ccae8aed07b50524c30e004cc6c6e9e9b8
James Hong <james.hong@cs.stanford.edu>
2023-08-04 02:01:35 -0700

    Add make

--- pacman_setup.sh
+++ pacman_setup.sh
@@ -6,10 +6,14 @@ sudo pacman-key --init
 sudo pacman-key --populate archlinux
 sudo pacman -Sy archlinux-keyring
 
-sudo pacman -S gcc cmake glibc linux-api-headers
+sudo pacman -S make
+
+# sudo pacman -S gcc cmake glibc linux-api-headers
 
 sudo pacman -S --overwrite "/etc/vpnc/vpnc-script" openconnect
 
-sudo pacman -S imagemagick evince neofetch stress net-tools traceroute
+sudo pacman -S --overwrite "*" neofetch net-tools traceroute dnsutils
+
+sudo pacman -S imagemagick
 
 sudo pacman -S opera opera-ffmpeg-codecs
